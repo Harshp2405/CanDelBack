12ca42b32e0f2178a0936f19ef656bdb47f4c42a
panara.harsh <panara.harsh@silvertouch.com>
2026-05-25 09:48:40 +0530

    Changes

--- Server.js
+++ Server.js
@@ -1,20 +1,20 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import "dotenv/config";
 
 import budgetRoutes from "./Routes/budget.routes.js";
 import expenseRoutes from "./Routes/expense.routes.js";
 
-const app = express();
+const dburl = process.env.Railway_Mongo_Uri || "mongodb://mongo:LcIvxrERnKWOzZvYGhZLcOgWlMUToeZt@kodama.proxy.rlwy.net:55528";
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose
-  .connect(
-    "mongodb://mongo:LcIvxrERnKWOzZvYGhZLcOgWlMUToeZt@kodama.proxy.rlwy.net:55528",
-  )
-  .then(() => console.log("MongoDB Connected"));
+mongoose.connect(dburl)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.error("MongoDB Connection Error:", err));
 
 app.use("/budget", budgetRoutes);
 app.use("/expenses", expenseRoutes);
